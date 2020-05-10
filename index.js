d5f3dcfa4e230d001d0576cbdff13158a3c74675
German Gomez Urbina <grgomezu@pop-os.localdomain>
2020-05-10 14:41:54 -0600

    index.js - Has code of the DJ bot - At the moment it can only greet. config.json - Configuration file where the token and prefix is passed in.

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+// pass in token through config
+// got some ideas from: https://gist.github.com/eslachance/3349734a98d30011bb202f47342601d3
+const config = require('./config.json');
+
+client.once('ready', () => {
+    console.log('I am ready to play some beats!');
+});
+
+client.on('message', async message => {
+
+    if (message.author.bot) return;
+    if (message.content.indexOf(config.prefix) !== 0) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'hey') {
+        message.channel.send('Hey buddy! Wanna hear some music?');
+    }
+});
+
+client.login(config.token);
