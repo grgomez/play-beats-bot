1d3ae46d484f4cc30c504eefb0b40d775ba4ec18
German Gomez Urbina <grgomezu@pop-os.localdomain>
2020-05-11 21:58:27 -0600

    Added some libraries and now I can play music through youtube!

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+/*
+	This is a DJ bot to play some music
+	@author: German Rafael Gomez Urbina <grgomezu@gmail.com>
+
+	Resources:
+		- https://gist.github.com/eslachance/3349734a98d30011bb202f47342601d3
+		- https://discord.js.org/#/docs/main/stable/topics/voice
+		- https://blog.pragmatists.com/let-your-javascript-variables-be-constant-1633e56a948d
+*/
+
 const Discord = require('discord.js');
-const client = new Discord.Client();
-// pass in token through config
-// got some ideas from: https://gist.github.com/eslachance/3349734a98d30011bb202f47342601d3
+const ytdl = require('ytdl-core');
+
+/* pass in token through config */
 const config = require('./config.json');
+const client = new Discord.Client();
+let connection = undefined; 
 
 client.once('ready', () => {
 	console.log('I am ready to play some beats!');
@@ -18,6 +30,22 @@ client.on('message', async message => {
 
 	if (command === 'hey') {
 		message.channel.send('Hey buddy! Wanna hear some music?');
+	}
+
+	if (command === 'join') {
+		if (message.member.voice.channel) {
+			if (connection === undefined) {
+				connection = await message.member.voice.channel.join();
+			}
+		}
+		else {
+			message.reply('Hey broski! Join a voice channel first!');
+		}
+	}
+
+	if (command === 'play') {
+		/* For now... I WILL SURVIVE!! */
+		connection.play(ytdl('https://www.youtube.com/watch?v=gYkACVDFmeg', { filter: 'audioonly' }));
 	}
 });
 
