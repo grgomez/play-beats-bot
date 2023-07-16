6cfc1096c4657b7e175cae0f797a52da7f25e8da
German R. Gomez Urbina <grgomezu@gmail.com>
2023-07-15 18:12:28 -0600

    Added support for slash commands and the necessary events

--- src/types/events.ts
+++ src/types/events.ts
@@ -8,7 +8,7 @@ export interface EventProps {
 
 export type EventKeys = keyof ClientEvents;
 export type EventExec<T extends EventKeys> = 
-	(props: EventProps, ...args: ClientEvents[T]) => Awaitable<void>;
+	(props: EventProps, ...args: ClientEvents[T]) => Awaitable<unknown>;
 export interface Event<T extends EventKeys> {
 	id: T
 	exec: EventExec<T>
--- src/utils/event.ts
+++ src/utils/event.ts
@@ -1,18 +1,28 @@
-import { Event, EventExec, EventKeys } from '../types';
-import { Client } from 'discord.js';
+import { Event, EventExec, EventKeys } from '../types'
+import { Client } from 'discord.js'
 
 export function event<T extends EventKeys>(id: T, exec: EventExec<T>): Event<T> {
 	return {
 		id,
 		exec,
-	};
-};
+	}
+}
 
 export function registerEvents(client: Client, events: Event<any>[]): void {
 	for (const event of events)
-		client.on(event.id, event.exec.bind(null, {
-			client,
-			log: (...args: any) =>
-				console.log(`[${event.id}]`, ...args)
-		}));
-};
+		client.on(event.id, async (...args) => {
+			// Create Props
+			const props = {
+				client,
+				log: (...args: unknown[]) =>
+					console.log(`[${event.id}]`, ...args)
+			}
+
+			// Catch uncaught errors
+			try {
+				await event.exec(props, ...args)
+			} catch(error) {
+				props.log('Uncaught Error', error)
+			}
+		})
+}
